ebcc6aec802f728509f676959226709adc8738d7
Lavanya537-coder <nadipallilavanya1234@gmail.com>
2026-06-13 21:48:21 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Dashboard from "./pages/Dashboard";
 import CreateTicket from "./pages/CreateTicket";
 import TicketList from "./pages/TicketList";
